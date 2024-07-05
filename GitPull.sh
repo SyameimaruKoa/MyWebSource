ffcbd2ddf84e4841814b60d67059b08929a21f21
syameimarukoa <toho.komeizi.aya@gmail.com>
2024-07-05 13:38:47 +0900

    2024-07-05に自動Push

--- GitPull.sh
+++ GitPull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /var/www
-git fetch origin && git reset --hard origin/main
+git fetch origin
+git reset --hard origin/main
 git -c core.sshCommand="ssh -i /root/.ssh/github" pull origin main
