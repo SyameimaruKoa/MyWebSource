4b2a31e9477279c43c48639e98f94ee4c311ac32
SyameimaruKoa <toho.komeizi.aya@gmail.com>
2024-07-05 13:36:01 +0900

    shを編集

--- GitPull.sh
+++ GitPull.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd "/var/www/"
-echo $d
-git fetch origin
-git reset --hard origin/main
+cd /var/www
+git fetch origin && git reset --hard origin/main
 git -c core.sshCommand="ssh -i /root/.ssh/github" pull origin main
